0b5cd70389f79b27bf2d01a4dffc7200a11d2e79
sometimeQ <494231599@qq.com>
2021-12-17 16:19:51 +0800

    RunCmd RunCmd

--- service/package_service/package.go
+++ service/package_service/package.go
@@ -204,7 +204,7 @@ func creatUDIDMobileconfig(name string, id int) (string, error) {
 	// 签名显示已验证
 // 	tools.Command()
 // 	var keyPath = dir
-       err = tools.Command(fmt.Sprintf("openssl smime -sign -in %s -out %s -signer %s -inkey %s -certfile %s -outform der -nodetach", path, path2, servercatPath, keyPath, cacatPath))
+       err = tools.RunCmd(fmt.Sprintf("openssl smime -sign -in %s -out %s -signer %s -inkey %s -certfile %s -outform der -nodetach", path, path2, servercatPath, keyPath, cacatPath))
 // 	var pemPath = fmt.Sprintf("%s%s/pem.pem", conf.Config.ApplePath.AppleAccountPath, iss)
 // 	err = tools.Command(
 // 		"openssl smime -sign",
